9d2cf913eeb9c03db23586ffde1ce9d089f3dea9
trongnhanle <letrongnhan1410200@gmail.com>
2021-07-14 17:20:13 +0700

    .

--- 1811061325_LeTrongNhan_BigSchool/Controllers/HomeController.cs
+++ 1811061325_LeTrongNhan_BigSchool/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace _1811061325_LeTrongNhan_BigSchool.Controllers
             {
                 UpcommingCourses = upcommingCourses,
                 ShowAction = User.Identity.IsAuthenticated,
-                Followings = _dbContext.Followings.Where(f => userId != null && f.FolloweeId == userId).ToList(),
+                Followings = _dbContext.Followings.Where(f => userId != null && f.FollowerId == userId).ToList(),
                 Attendances = _dbContext.Attendances.Include(a => a.Course).Where(a => userId != null && a.AttendeeId == userId).ToList()
 
             };
